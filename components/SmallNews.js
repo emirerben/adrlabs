44aec7e7b38876df7ed617047524104556eed9b7
erben <erben@umich.edu>
2022-10-03 15:48:45 -0400

    Updated news, but it is lacking the right width allignment

--- components/SmallNews.js
+++ components/SmallNews.js
@@ -8,23 +8,22 @@ const SmallNews = () => (
     <h2>NEWS</h2>
     <div className={utilStyles.breakerBlack}></div>
     <div className={utilStyles.NewsContainerStyle}>
+        <div className={utilStyles.breakerBlack}></div>
         <div className={utilStyles.NewsBlockStyle}>
             <h3>Keynote Lecture International Manufacturing Forum Series 2021</h3>
-            <p>3/31/21</p>
-            <p>READ MORE</p>
+            <p>3/31/21 <br></br><br></br>READ MORE</p>
+            <div className={utilStyles.yellowHighlight}></div>
             
         </div>
         <div className={utilStyles.breakerBlack}></div>
-        <div className={utilStyles.TopicBlockStyle}>
+        <div className={utilStyles.NewsBlockStyle}>
             <h3>Robotic Construction</h3>
-            <p>3/31/21</p>
-            <p>READ MORE</p>
+            <p>3/31/21 <br></br><br></br>READ MORE</p>
         </div>
         <div className={utilStyles.breakerBlack}></div>
-        <div className={utilStyles.TopicBlockStyle}>
+        <div className={utilStyles.NewsBlockStyle}>
             <h3>Human-Robot Collaboration</h3>
-            <p>3/31/21</p>
-            <p>READ MORE</p>
+            <p>3/31/21 <br></br><br></br>READ MORE</p>
         </div>
         
     </div>
