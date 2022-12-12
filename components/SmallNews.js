26f3e267ae762a1bf891444a301382fa8a85b33b
erben <erben@umich.edu>
2022-12-11 22:07:44 -0500

    updated publications for mobile, news on home page

--- components/SmallNews.js
+++ components/SmallNews.js
@@ -10,20 +10,20 @@ const SmallNews = () => (
     <div className={utilStyles.NewsContainerStyle}>
         <div className={utilStyles.breakerBlack}></div>
         <div className={utilStyles.NewsBlockStyle}>
-            <h3>Keynote Lecture International Manufacturing Forum Series 2021</h3>
-            <p>3/31/21 <br></br><br></br>READ MORE</p>
+        <h3>Robotically Fabricated Structure Receives Global Future Design Award</h3>
+            <p>10/31/22 <br></br><br></br><Link href={'/news/globaldesignaward'}>READ MORE</Link></p>
             <div className={utilStyles.yellowHighlight}></div>
             
         </div>
         <div className={utilStyles.breakerBlack}></div>
         <div className={utilStyles.NewsBlockStyle}>
-            <h3>Robotic Construction</h3>
-            <p>3/31/21 <br></br><br></br>READ MORE</p>
+        <h3>RFS Awarded the Honorable Mention for the Loop Design Awards 2022</h3>
+            <p>10/05/22 <br></br><br></br><Link href={'/news/loopdesignawards'}>READ MORE</Link></p>
         </div>
         <div className={utilStyles.breakerBlack}></div>
         <div className={utilStyles.NewsBlockStyle}>
-            <h3>Human-Robot Collaboration</h3>
-            <p>3/31/21 <br></br><br></br>READ MORE</p>
+        <h3>RFS Nominated for the LOOP Design Awards 2022 People's Choice</h3>
+            <p>08/29/22 <br></br><br></br><Link href={'/news/peopleschoice'}>READ MORE</Link></p>
         </div>
         
     </div>
