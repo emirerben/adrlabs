54f879dadde8086fbd2a6c8d1218f04276a32619
erben <erben@umich.edu>
2023-03-23 15:57:27 -0400

    started projects

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,10 +1,104 @@
 import UnderConstruction from "../../components/UnderConstruction";
 import CustomCursor from "../../components/Cursor";
+import utilStyles from '../../styles/utils.module.css';
+import Header from "../../components/Header";
+import Link from "next/link";
+import firstpic from '../../public/images/latestwork1.jpeg'
+import secondpic from '../../public/images/latestwork2.jpg'
+import thirdpic from '../../public/images/latestwork3.jpg'
+import fourthpic from '../../public/images/latestwork4.jpeg'
+import Image from "next/image";
+
 export default function Projects(){
     return(
         <>
         <CustomCursor />
-        <UnderConstruction></UnderConstruction>
+        <div className={utilStyles.headerWhiteBackground}></div>
+        <Header></Header>
+        <div className={utilStyles.TitleSection}>
+            <h1>PROJECTS</h1>
+            <p style={{letterSpacing:-1, fontSize: 30, width: 600, fontWeight: 300, }}>ADR is an interdisciplinary laboratory for advanced research in Architecture, Computational Design, and Robotic Construction.</p>
+        </div>
+        <div style={{paddingLeft:40, marginTop:100}} className={utilStyles.lastestWorkStyle}>
+            <div style={{gap:60}} className={utilStyles.projectsScrollStyle}>
+                <div className={utilStyles.projectCardStyle}>
+                    <Link href="/">
+                        <Image className={utilStyles.projectImageStyle}
+                            src={firstpic}
+                            alt="project"
+                            width={360}
+                            height={784}
+                        />
+                    </Link>
+                    <div className={utilStyles.titleArrowLatestWork}>
+                        <p>Robotically Fabricated Structure 2.0</p>
+                        <Image className={utilStyles.smallUpperArrow}
+                            src={"/images/ADR-GraphicElements_vcopy.png"}
+                            alt="Logo"
+                            width={40}
+                            height={40}
+                        />
+                    </div>
+                </div>
+                <div className={utilStyles.projectCardStyle}>
+                    <Link href="/">
+                        <Image className={utilStyles.projectImageStyle}
+                            src={secondpic}
+                            alt="project"
+                            width={300}
+                            height={254}
+                        />
+                    </Link>
+                    <div className={utilStyles.titleArrowLatestWork}>
+                        <p>Robotically Fabricated Structure <br></br>(RFS)</p>
+                        <Image className={utilStyles.smallUpperArrow}
+                            src={"/images/ADR-GraphicElements_vcopy.png"}
+                            alt="Logo"
+                            width={40}
+                            height={40}
+                        />
+                    </div>
+                </div>        
+                <div className={utilStyles.projectCardStyle}>
+                    <Link href="/">
+                        <Image className={utilStyles.projectImageStyle}
+                            src={thirdpic}
+                            alt="project"
+                            width={300}
+                            height={391}
+                        />
+                    </Link>
+                    <div className={utilStyles.titleArrowLatestWork}>
+                        <p>Muti-Robotic Timber Assembly</p>
+                        <Image className={utilStyles.smallUpperArrow}
+                            src={"/images/ADR-GraphicElements_vcopy.png"}
+                            alt="Logo"
+                            width={40}
+                            height={40}
+                        />
+                    </div>
+                </div>
+                {/* <div className={utilStyles.projectCardStyle}>
+                    <Link href="/">
+                        <Image className={utilStyles.projectImageStyle}
+                            src={fourthpic}
+                            alt="project"
+                            width={360}
+                            height={287}
+                        />
+                    </Link>
+                    <div className={utilStyles.titleArrowLatestWork}>
+                        <p>DFAB House</p>
+                        <Image className={utilStyles.smallUpperArrow}
+                            src={"/images/ADR-GraphicElements_vcopy.png"}
+                            alt="Logo"
+                            width={40}
+                            height={40}
+                        />
+                    </div>
+                </div>           */}
+            </div>
+        </div>
         </>
     );
 
