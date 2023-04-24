507d833719010179424496ec36d930d43f73143b
erben <erben@umich.edu>
2023-04-24 18:05:53 -0400

    newslines

--- components/SmallNews.js
+++ components/SmallNews.js
@@ -8,19 +8,18 @@ const SmallNews = () => (
     <h2 style={{fontWeight: "500"}}>RECENT NEWS</h2>
     <div className={utilStyles.breakerBlack}></div>
     <div className={utilStyles.NewsContainerStyle}>
-        <div className={utilStyles.breakerBlack}></div>
         <div className={utilStyles.NewsBlockStyle}>
         <h3>Robotically Fabricated Structure Receives Global Future Design Award</h3>
             <p>10/31/22 <br></br><br></br><Link href={'/news/globaldesignaward'}>READ MORE</Link></p>
             <div className={utilStyles.yellowHighlight}></div>
             
         </div>
-        <div className={utilStyles.breakerBlack}></div>
+        <div className={utilStyles.verticalLine}></div>
         <div className={utilStyles.NewsBlockStyle}>
         <h3>RFS Awarded the Honorable Mention for the Loop Design Awards 2022</h3>
             <p>10/05/22 <br></br><br></br><Link href={'/news/loopdesignawards'}>READ MORE</Link></p>
         </div>
-        <div className={utilStyles.breakerBlack}></div>
+        <div className={utilStyles.verticalLine}></div>
         <div className={utilStyles.NewsBlockStyle}>
         <h3>RFS Nominated for the LOOP Design Awards 2022 People's Choice</h3>
             <p>08/29/22 <br></br><br></br><Link href={'/news/peopleschoice'} className={utilStyles.underlineHover}>READ MORE</Link></p>
