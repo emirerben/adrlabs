b39f522f1fbc52d8b5851b6ad582e38d3ab85dbb
erben <erben@umich.edu>
2023-05-15 14:06:49 -0400

    update

--- api/images.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export default function handler(req, res) {
-  const imagesDir = path.join(process.cwd(), 'public', 'images');
-  const imageFilenames = fs.readdirSync(imagesDir);
-    console.log(imageFilenames)
-  res.status(200).json(imageFilenames);
-}
